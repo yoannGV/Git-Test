57c0e7c9011513572ebc9900e2b8d749efff6782
yoannGV <yoann.GV@hotmail.com>
2022-11-24 13:28:50 +0000

    Number range input added of RandomUuid function

--- RandomUuid.py
+++ RandomUuid.py
@@ -5,7 +5,7 @@ import datetime
 file_Name = str(input("Please input a file path: "))
 file_timestamp = datetime.datetime.now().strftime('%d-%m-%Y-%H:%M:%S-')
 output_File = file_timestamp + file_Name
-number = range(1000)
+number = range(int(input("Enter the number of UUID that you need: ")))
 
 def randomUuid(output_File, number):
     with open(output_File, 'w') as uuid_File:
@@ -15,7 +15,5 @@ def randomUuid(output_File, number):
 
 
 randomUuid(output_File, number)
-
-
 
 
